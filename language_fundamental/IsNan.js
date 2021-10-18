88627e692c020db153692d3c973b7d8386517e7f
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-10-18 23:14:55 +0600

    IsNan

---
+++ language_fundamental/IsNan.js
@@ -0,0 +1,14 @@
+let a = 5
+let b =4
+console.log(4*'aa')
+if( !isNaN(a*b)){
+    console.log('is number ')
+}else{
+    console.log('is Nan')
+}
+
+console.log(isFinite(a*"jahed")) // false
+console.log(isFinite(5*6)) //true
+console.log(isFinite(Number.MAX_VALUE *4)) //false
+console.log(isFinite(Infinity)) //false 
+
