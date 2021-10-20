080ca890886a1ba561f2da24f7fa32f3a54b828b
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-10-20 22:12:40 +0600

    Mark and sweep algorithm

---
+++ language_fundamental/Memory Management & Garbage Collaction/Garbage.js
@@ -0,0 +1,5 @@
+let obj= {
+    name: "jahed"
+}
+ obj = null;
+// then the first line is garbage collected
---
+++ language_fundamental/Memory Management & Garbage Collaction/Mark&Sweep.js
@@ -0,0 +1,5 @@
+// Mark and sweep algorithm
+
+// The algorithm starts from the root object and checks for the references linked 
+// * In case if this algorithm finds unreachable locations , then those are the locations to be removed
+
