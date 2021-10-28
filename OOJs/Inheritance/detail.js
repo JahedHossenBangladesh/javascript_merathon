a86e55d32148f94c319f00c9c702af0cb5a5bd00
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-10-28 23:33:58 +0600

    this is the inheritance

--- OOJs/Inheritance/detail.js
+++ OOJs/Inheritance/detail.js
@@ -1,15 +1,16 @@
 class Parent{
-    constructor(){
+    constructor(a){
+        this.a = a;
         console.log("Parent constructor");
     }
     parentMethod(){
-        console.log("Parent method");
+        console.log("Parent method",this.a);
     }
 }
 
 class Child extends Parent{}
 
-let obj = new Child();
+let obj = new Child(12);
 console.log(obj);
 
 obj.parentMethod();
